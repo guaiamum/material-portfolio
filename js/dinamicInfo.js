63e5b4e702d416094220058b671487e78cb9c101
Cainã Brazil <cainabrazil@gmail.com>
2018-02-23 09:26:59 +0000

    added skills + academia section

--- js/dinamicInfo.js
+++ js/dinamicInfo.js
@@ -11,15 +11,43 @@ function loadInfo(user){
   });
 
   // SKILLS
-  var skillsHave = $('#skills-have');
+  var skillsContainer = $('#skills-have');
   user.skills.forEach((e) => {
-    skillsHave.append(
+    skillsContainer.append(
       $('<div>',{class:'skill tooltipped','data-position':'bottom','data-tooltip': e.description}).append(
         $('<div>',{class:'skill-content'}).append(
           $('<div>',{class:'center-flex',html: e.title}))));
   });
 
+  skillsContainer = $('#skills-want');
+  user.skillsWanted.forEach((e) => {
+    skillsContainer.append(
+      $('<div>',{class:'skill tooltipped','data-position':'bottom','data-tooltip': e.description}).append(
+        $('<div>',{class:'skill-content'}).append(
+          $('<div>',{class:'center-flex',html: e.title}))));
+  });
 
   //Initializes tooltipped's object's
-  $('.tooltipped').tooltip({delay: 50});
+  $('.tooltipped').tooltip({delay: 15});
+
+// EXPERIENCES
+  user.jobs.forEach((e,i) => {
+    var container = $('#experiences');
+
+    var div = $('<div>',{class: 'xp col'})
+    var title = $('<h5>',{ html: e.subtitle });
+
+    var map = '<i class="tiny material-icons">location_on</i> ';
+    var location = $('<p>',{ class: 'job-location', html: map + e.company + ', ' + e.location });
+
+    var calendar = '<i class="tiny material-icons">date_range</i> ';
+    var duration = $('<p>',{ class: 'job-duration', html: calendar + e.dateBegin +' - '+ e.dateEnd });
+
+    var description = $('<p>',{ class: 'job-description',html: e.description });
+    var tasks =  $('<ul>', { class: 'job-tasks'});
+    e.tasks.forEach((task) => {var currTask = $('<li>',{ html: task }); tasks.append(currTask);});
+
+    container.append(div.append(title,'<hr/>',location,duration,description.append(tasks)));
+  });
+
 }
---
+++ js/me.js
@@ -0,0 +1,91 @@
+var me = {
+  // PROFILE
+  name: "Cainã Brazil",
+  subtitle: "Undergraduate in Computer Engineering",
+  thumbnail: "../../Dropbox/Pictures/perfis/perfil_face-SQUARE.jpg",
+  links: [{
+      id: '#github-link',
+      href: "https://github.com/guaiamum"
+    },
+    {
+      id: '#instagram-link',
+      href: "https://www.instagram.com/_guaiamum/"
+    },
+    {
+      id: '#medium-link',
+      href: "https://medium.com/@cainbrazil"
+    },
+    {
+      id: '#linkedin-link',
+      href: "https://www.linkedin.com/in/cainã-brazil-586379114/"
+    },
+  ],
+  // SKILLS
+  skills: [{
+      title: 'C#',
+      description: '3 years. Game scripts and Web development, Api\'s, etc...'
+    },
+    {
+      title: '.NET',
+      description: '2 years. Web development, mainly with E.R.P.\'s'
+    },
+    {
+      title: '<img src="images/skills/have/xamarin.png" alt="Xamarin"/>',
+      description: 'Xamarin: 1 month. Maintenance in app developed for a big company'
+    },
+    {
+      title: 'API',
+      description: '1 year. Created and maintained some API\'s for mobile comsumption'
+    },
+    {
+      title: 'PHP',
+      description: '1 month. Web development in a start-up company'
+    },
+    {
+      title: 'Yii',
+      description: '1 month. Web development in a start-up company'
+    },
+    {
+      title: 'JS',
+      description: '3 years. Web page scripts and mobile development'
+    },
+    {
+      title: '<img src="images/skills/have/reactNative.png" alt="React Native"/>',
+      description: 'React Native: 1 month. Simple apps for experimenting'
+    },
+  ],
+  skillsWanted: [{
+    title: '<img src="images/skills/want/python.png" alt="Python"/>',
+    description: 'Python: seems like a "do it all" simple programming language, with lots of frameworks'
+  }, ],
+  // EXPERIENCES
+  jobs: [{
+      company: 'Open System',
+      subtitle: 'Software development internship',
+      dateBegin: 'Jun 2017',
+      dateEnd: 'Feb 2018',
+      location: 'Salvador, BA',
+      description: 'Here I helped to develop great solutions to clients, and to keep and raise the name of a company that has more than 25 years in the market providing integrated mobile solutions for big retail companies.',
+      tasks: ['Modeling the database using SQL Server and Code-first Migrations', 'ASP.NET MVC development and multi-layer architecture', 'Front-end design using variable plugins in JavaScript, CSS and Bootstrap, etc', 'Developed WebAPI to provide data to mobile devices connected and receive information from other APIs', 'Full-stack development for mobile devices (android) using Xamarin']
+      // 'Initial development of big international, multi-language web-system, with mobile interface (C# Asp.Net MVC5 Xamarin SQLServer)'
+    },
+    {
+      company: 'Viva inovação',
+      subtitle: 'Web development internship',
+      dateBegin: 'Mar 2017',
+      dateEnd: 'Jun 2017',
+      location: 'Salvador, BA',
+      description: 'Viva Inovação is a startup company that develops technology solutions focused on increasing productivity.',
+      tasks: ['Learned basics MVC architecture and front-end developing using Razor, jQuery, Bootstrap, etc', 'Back-end development using C# with ASP.NET and PHP with Yii framework', 'Talked directly to costumers and provided support', 'Managed deployments and maintenance on server applicatications']
+      // 'Great progress in a major project inside the company (C# Asp.Net MVC5 SQLServer)','Maintenance in PHP Yii framework project (MySQL PhpMyAdmin)'
+    }, {
+      company: 'Ufba',
+      subtitle: 'Game development internship',
+      dateBegin: 'Mar 2016',
+      dateEnd: 'Mar 2017',
+      location: 'Salvador, BA',
+      description: 'Multi-subject Federal University project',
+      tasks: ['From ground zero made an entire virtual world of my University Campus, including terrain, buildings, streets and main architecture characteristics', 'Participants from different areas of knowledge, like: arts, architecture, computer science', 'Used Sketchup 3D to convert the work done by architecture students to models that could be used in the Unity 3D platform', 'Used terrain plugins to create roads and adapt the terrain imported from the city database to the new building that were being put \'on top of it\'', 'Developed my C# skills with scripts to control the user inputs and animations around the Campus']
+    },
+  ],
+} //END ME
